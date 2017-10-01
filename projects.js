d7bd013d09ee9e32538d33808310b4a46ba4f15a
Simone Di Fonzo <difonzosimone@hotmail.com>
2017-10-01 22:10:56 +0300

    1.1.1-unreleased
    - Fixed linking;
    - Removed "iframe" element from "index.html";
    - Added "projects.html";
    - NEED to fix load-in issues to "projects.html"

--- projects.js
+++ projects.js
@@ -1,39 +1,38 @@
 filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
 
-$("all").on("click", filterSelection('all'));
-$("web").on("click", filterSelection('web'));
-$("c++").on("click", filterSelection('c++'));
-$("electron").on("click", filterSelection('electron'));
-
-var filter,
-
-function AddClass(element, name) {
+// Show filtered elements
+function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
   }
 }
 
-function RemoveClass(element, name) {
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
     }
   }
   element.className = arr1.join(" ");
 }
 
-function filterSelection(filter) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-  }
-}
+filterSelection("all")
