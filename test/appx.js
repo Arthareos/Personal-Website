0f3577c5626c1c8fb7714edf50977b283b9eacf7
Simone Di Fonzo <difonzosimone@hotmail.com>
2018-06-09 21:24:07 +0300

    v1.3.2-beta
     - "test" set up;

---
+++ test/appx.js
@@ -0,0 +1,47 @@
+const $ = require('jquery');
+
+let searchBar = 0;  
+
+function minimize() {
+    win.minimize();
+};
+
+function maximize() {
+    if(!win.isMaximized()) {
+        win.maximize();
+    }
+    else
+    {
+        win.unmaximize();
+    }
+};
+
+function closed() {
+    win.close();
+};
+
+function tabSelector(page, id) {
+    var iframe = $("#main-content");
+    var link = document.getElementById(id);
+    $("#mainimg").attr("src", "./Style/Images/Navbar/logo.png");
+    $("#brow").attr("src", "./Style/Images/Navbar/logo.png");
+    $("#rad").attr("src", "./Style/Images/Navbar/logo.png");
+    $("#lib").attr("src", "./Style/Images/Navbar/logo.png");
+    if (id == "#mainimg") {
+        $(link).attr("src", "./Style/Images/Close/2.png");
+    }
+    if (id == "#brow") {
+        $(link).attr("src", "./Style/Images/Close/2.png");
+    }
+    if (id == "#rad") {
+        $(link).attr("src", "./Style/Images/Close/2.png");
+    }
+    if (id == "#lib") {
+        $(link).attr("src", "./Style/Images/Close/2.png");
+    }
+    iframe.attr("src", page);
+}
+
+function onLoad() {
+    tabSelector('backup.html', 'mainimg');
+}
