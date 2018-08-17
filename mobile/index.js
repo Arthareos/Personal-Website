2a64bca4450de7dd4a5ccc5f165657ceb3f65f17
Simone Di Fonzo <difonzosimone@hotmail.com>
2018-08-17 17:33:43 +0300

    v1.5.3-master
     -little fixes here and there;

--- mobile/index.js
+++ mobile/index.js
@@ -1,31 +1,4 @@
-let action = 0;
 let open = 0;
-
-function tabSelector(page, id) {
-    var iframe = $("#main-content");
-    var link = document.getElementById(id);
-    $("#mainimg").attr("src", "./style/img/logo.png");
-    $("#brow").attr("src", "./style/img/logo.png");
-    $("#rad").attr("src", "./style/img/logo.png");
-    $("#lib").attr("src", "./style/img/up.png");
-    if (id == "#mainimg") {
-        $(link).attr("src", "./style/img/");
-    }
-    if (id == "#brow") {
-        $(link).attr("src", "./style/img/");
-    }
-    if (id == "#rad") {
-        $(link).attr("src", "./style/img/");
-    }
-    if (id == "#lib") {
-        $(link).attr("src", "./style/img/");
-    }
-    iframe.attr("src", page);
-}
-
-function onLoad() {
-    tabSelector('backup.html', 'mainimg');
-}
 
 function operateMenu() {
     if (open == 0) {
